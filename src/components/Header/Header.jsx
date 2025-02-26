4911df76a3d56738bc5c52cd3273b192366bae53
Richard Oppong-Konadu <richardok@gmail.com>
2025-02-26 14:59:46 +0000

    add fonts and variables

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "../Header/Header.scss";
+
+export default function Header() {
+  return (
+    <nav className="nav">
+      <h1>Snaps</h1>
+      <button>filter</button>
+    </nav>
+  );
+}
