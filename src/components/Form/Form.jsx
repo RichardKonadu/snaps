169351e4b63eb21ccee5e2f738957f1f567a942a
Richard Oppong-Konadu <richardok@gmail.com>
2025-03-09 11:37:41 +0000

    add form validation error styling

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,74 +2,92 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 const API_KEY = "4eca160b-03d9-48da-9c9b-80ca921f8809";
+import "../Form/Form.scss";
+
+const initialFormData = {
+  name: "",
+  comment: "",
+};
 
 export default function Form({ fetchComments }) {
-  const [formData, setFormData] = useState({
-    name: "",
-    comment: "",
-  });
-
-  const [message, setMessage] = useState(false);
+  const [formData, setFormData] = useState(initialFormData);
+  const [formErrors, setFormErrors] = useState({});
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   const { photoId } = useParams();
 
-  const postComment = async (name, comment) => {
-    try {
-      const response = await axios.post(
-        `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${photoId}/comments?api_key=${API_KEY}`,
-        { name, comment }
-      );
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  const handleChange = (e) => {
+  const changeHandler = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
   };
 
-  //  how do I make it so the new comments show without the page refreshing
-
-  const handleSubmit = async (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    if (!formData.name || !formData.comment) {
-      setMessage("Please fill in all form fields");
+    setFormSubmitted(false);
+
+    let formIsValid = true;
+    const errors = {};
+
+    if (!formData.name) {
+      formIsValid = false;
+      errors["name"] = "You must enter a name for your comment";
+    }
+    if (!formData.comment) {
+      formIsValid = false;
+      errors["comment"] = "You must enter a comment for your post";
+      console.log(errors);
+    }
+    if (!formIsValid) {
+      setFormErrors(errors);
       return;
     }
-    setFormData({ name: "", comment: "" });
-    await postComment(formData.name, formData.comment);
-    await fetchComments();
+
+    try {
+      await axios.post(
+        `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${photoId}/comments?api_key=${API_KEY}`,
+        formData
+      );
+      setFormSubmitted(true);
+      setFormData(initialFormData);
+      setFormErrors({});
+      fetchComments();
+    } catch (error) {
+      throw error;
+    }
   };
 
-  useEffect(() => {
-    setFormData({ name: "", comment: "" });
-  }, []);
-
   return (
-    <form onSubmit={handleSubmit} className="comment__form" action="">
+    <form onSubmit={submitHandler} className="comment__form" action="">
       <label className="comment__form__label">
         Name{" "}
         <input
           name="name"
           type="text"
           value={formData.name}
-          onChange={handleChange}
+          onChange={changeHandler}
+          className={`form__input ${
+            formErrors.name ? "form__input--error" : ""
+          }`}
         />
+        {formErrors.name && <p className="error">{formErrors.name}</p>}
       </label>
       <label className="comment__form__label">
         Comment{" "}
-        <input
+        <textarea
           name="comment"
           type="text"
           value={formData.comment}
-          onChange={handleChange}
-        />
+          onChange={changeHandler}
+          className={`form__input ${
+            formErrors.comment ? "form__input--error" : ""
+          }`}
+        ></textarea>
+        {formErrors.comment && <p className="error">{formErrors.comment}</p>}
       </label>
       <button className="comment__form__btn">Submit</button>
-      <p>{message && message}</p>
+      {/* <p>{message && message}</p> */}
     </form>
   );
 }
